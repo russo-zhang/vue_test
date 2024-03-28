097b2ec3ba0715d844c84c9adb6ca5efb248a639
russo.zhang <russo.zhang@foobargame.com>
2024-03-28 17:46:27 +0800

    国内平台分享

--- src/views/mobile/Home.vue
+++ src/views/mobile/Home.vue
@@ -11,9 +11,18 @@
                 <li>
                     <el-button type="primary" @click="lineShare">Line Share</el-button>
                 </li>
-
                 <li>
                     <el-button @click="whatsAppShare">WhatsApp Share</el-button>
+                </li>
+                <el-devider></el-devider>
+                <!-- <li>
+                    <el-button type="primary" @click="weChatShare">WeChat Share</el-button>
+                </li> -->
+                <li>
+                    <el-button @click="baiduShare">百度贴吧 分享</el-button>
+                </li>
+                <li>
+                    <el-button type="primary" @click="weiboShare">微博 分享</el-button>
                 </li>
             </ul>
         </main>
@@ -22,6 +31,9 @@
 
 <script lang="ts" setup>
 const shareUrl = "https://www.vue-test.site";
+const title = "share title example";
+const shareText = "share text example";
+const sharePic = "https://www.vue-test.site/favicon.png";
 const twitterShare = () => {
     const text = "This is share text example";
     const via = "Your Twitter username example";
@@ -61,6 +73,28 @@ const lineShare = () => {
 const whatsAppShare = () => {
     const text = "This is share text example";
     const intentUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)}&url=${encodeURIComponent(shareUrl)}`;
+    openShareWindow(intentUrl);
+};
+/* const weChatShare = () => {
+        wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: '', // 必填，公众号的唯一标识
+        timestamp: , // 必填，生成签名的时间戳
+        nonceStr: '', // 必填，生成签名的随机串
+        signature: '',// 必填，签名
+        jsApiList: [] // 必填，需要使用的JS接口列表
+    });
+}; */
+const baiduShare = () => {
+    const intentUrl = `http://tieba.baidu.com/f/commit/share/openShareApi?title=${title}&url=${encodeURIComponent(
+        shareUrl,
+    )}&pic=${encodeURIComponent(sharePic)}`;
+    openShareWindow(intentUrl);
+};
+const weiboShare = () => {
+    const intentUrl = `http://service.weibo.com/share/share.php?url=${encodeURIComponent(shareUrl)}&title=${encodeURIComponent(
+        title,
+    )}&pic=${encodeURIComponent(sharePic)}`;
     openShareWindow(intentUrl);
 };
 </script>
