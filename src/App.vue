0400ab751ec0d1e46c94d1182571d9a111d6db32
russo_zhang <russo_zhang@163.com>
2024-10-24 12:24:45 +0800

    checkSystemUpdate

--- src/App.vue
+++ src/App.vue
@@ -16,41 +16,57 @@ export default defineComponent({
         //根据ip禁用页面逻辑
         const { isShowPage } = useBan();
         let lastModified = "";
-        const interval = 1000 * 10;
-
-        const checkEtag = async () => {
-            const res = await axios(`/index.html`, {
-                method: "head",
-                headers: {
-                    "Cache-Control": "no-cache",
-                },
-            });
-            console.log("res.headers:", res.headers);
-            console.log("last-modified:", res.headers["last-modified"]);
-            if (!lastModified) {
-                lastModified = res.headers["last-modified"];
-                setTimeout(checkEtag, interval);
-            } else if (lastModified !== res.headers["last-modified"]) {
-                ElMessageBox.confirm("检测到系统有更新，是否继续?", "Warning", {
-                    confirmButtonText: "确定",
-                    cancelButtonText: "稍后提醒我",
+        /* 
+            检测系统更新
+            interval: 检测时间间隔,不传默认5分钟
+        */
+        const checkSystemUpdate = (minutes = 5) => {
+            const interval = 1000 * 60 * minutes;
+            // 提醒更新
+            const remindUpdate = () => {
+                ElMessageBox.confirm("检测到系统有更新，是否刷新页面?", "Warning", {
+                    confirmButtonText: "刷新",
+                    cancelButtonText: `稍后提醒我`,
                     type: "warning",
                 })
                     .then(() => {
+                        // 刷新页面
                         window.location.reload();
                     })
                     .catch(() => {
                         ElMessage({
-                            type: "info",
-                            message: "稍后提醒我",
+                            type: "warning",
+                            message: `将在${minutes}分钟后再次提醒更新`,
                         });
-                        setTimeout(checkEtag, interval);
+                        setTimeout(remindUpdate, interval);
                     });
-            } else {
-                setTimeout(checkEtag, interval);
+            };
+            // 检测最后修改时间
+            const checkLastModified = async () => {
+                const res = await axios(`/index.html`, {
+                    method: "head",
+                    headers: {
+                        "Cache-Control": "no-cache",
+                    },
+                });
+                if (!lastModified) {
+                    // 第一次检测
+                    lastModified = res.headers["last-modified"];
+                    setTimeout(checkLastModified, interval);
+                } else if (lastModified !== res.headers["last-modified"]) {
+                    // 更新了
+                    remindUpdate();
+                } else {
+                    // 未更新
+                    setTimeout(checkLastModified, interval);
+                }
+            };
+            // 生产环境检测
+            if (process.env.NODE_ENV === "production") {
+                checkLastModified();
             }
         };
-        checkEtag();
+        checkSystemUpdate(60 * 24);
         return {
             isShowPage,
         };
